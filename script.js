a9f6b9a76e583841172d258eadd7f0956db64572
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-19 13:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
 let element=document.getElementById("browser-info");
-element.innerHTML="You are using"+navigator.appName navigator.appVersion+"version";
+element.innerHTML="You are using "+navigator.appName+"version"+navigator.appVersion;
